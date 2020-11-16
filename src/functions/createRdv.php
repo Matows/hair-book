d695b9115b0e4660be86fa7321647c0ac3f4380e
romain22222 <romrom22222@hotmail.fr>
2020-11-16 09:37:40 +0100

    Update createRdv.php

--- src/functions/createRdv.php
+++ src/functions/createRdv.php
@@ -1,18 +1,19 @@
 <?php
 function createRdv($prestations, $rdvtime, $who="anonyme", $hairdresser="n'importe")
 {
+	global $db;
 	//format $rdvHour='yyyy-mm-dd hh:mm:00'
 	//format $prestation (une seule) : array("name" => name, "time" => time(min), "price" => price)
 	//format $prestations : [$prestation]
 	$presStockage="";
-	$totPrice=0;
-	$totTime=0;
+	$totalPrice=0;
+	$totalTime=0;
 	foreach ($prestations as $prestation) {
 		$presStockage=$presStockage.$prestation["name"].", ";
-		$totTime+=$prestation["time"];
-		$totPrice+=$prestation["price"];
+		$totalTime+=$prestation["time"];
+		$totalPrice+=$prestation["price"];
 	}
 	$presStockage=substr($presStockage, 0, strlen($presStockage)-3);
-	$sql="INSERT INTO `rdvs` (`id`, `prestations`, `maxprice`, `who`, `time(min)`, `hairdresser`, `rdvHour`) VALUES (NULL, $presStockage, $totPrice, $who, $totTime, $hairdresser, $rdvtime);";
-    mysqli_query($c, $sql);
+	$sql="INSERT INTO `rdvs` (`id`, `prestations`, `maxprice`, `who`, `time(min)`, `hairdresser`, `rdvHour`) VALUES (NULL, $presStockage, $totalPrice, $who, $totalTime, $hairdresser, $rdvtime);";
+    	mysqli_query($db, $sql);
 }
