220a0bfd05d2bc64cfb61c5f5420044a618470a7
romain22222 <romrom22222@hotmail.fr>
2020-12-01 00:11:56 +0100

    ajout presentation personnel

---
+++ src/views/presPers.php
@@ -0,0 +1,30 @@
+<div>
+	<h1>Présentation du personnel :</h1>
+	<div>
+		<div>
+			<h2>Directeur</h2>
+			<p>nomDirecteur</p>
+			<img src="../../img/directeur.jpg">
+		</div>
+		<div>
+			<h2>Employés :</h2>
+			<ul>
+				<li>
+					employé1
+				</li>
+				<li>
+					employé2
+				</li>
+				<li>
+					employé3
+				</li>
+				<li>
+					employé4
+				</li>
+				<li>
+					employé5
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
