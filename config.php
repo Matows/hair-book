d26cdd5985934f4fb8c99936b8f08f3ddd3c1d86
Zall9 <55153194+Zall9@users.noreply.github.com>
2020-12-01 16:32:55 +0100

    Config de CONFIG.PHP

--- config.php
+++ config.php
@@ -3,26 +3,72 @@
 /* Fichier de configuration à destination du gérant du salon
  *
  */
-
-$_horaires = ['8h-12h', '14h-17h'];
+$_website_name= 'HairBook';
+$_horaires = array[
+    'Lundi'=> ['8h-12h', '14h-17h'],
+    'Mardi'=>['8h-12h', '14h-17h'],
+    'Mercredi'=>['8h-12h', '14h-17h'],
+    'Jeudi'=>['8h-12h', '14h-17h'],
+    'Vendredi'=>['8h-12h', '14h-17h',
+    'Samedi'=>['8h-12h', '14h-17h'],
+    'Dimanche'=>['Fermé']
+                  ];
 // $_horaires = ['8h-18h'];
 
 $_prestations = [
-    'Coupe classique' => [
+    //coupes
+    'Coupe simple' => [
         'prix' => 10,
-        'type cheveux' => 'all'
+        'type cheveux' => 'all',
+        'description' => '',
     ],
     'Coloration' => [
         'prix' => 20,
-        'type cheveux' => [
-            'longeur' => ['moyen', 'long'],
-            'qualité' => 'all'
-        ]
-    ]
+        'type cheveux' => 'all',
+    ],
+    'Coupe - Coiffure'=> [
+    'prix' => 25,
+    'type cheveux' => 'all',
+    'description' => 'Coupe avec Shampoing et Coiffure ensuite',
+    ],
+    'Coupe - Coiffure - Soin Divers' => [
+        'prix' => 25,
+        'type cheveux' => 'all',
+        'description' => 'Coupe avec Shampoing, coiffure & soins divers des cheveux',
+    ],
+    
+    'Suppléments Couleurs' => [
+        'prix' => ['longueur'=>['9,15,21']],
+        'type cheveux' => 'all',
+        'description' => 'Coloration en supplément à une coupe',
+    ],
+    //Mèches 
+    'Mèches' => [
+        'prix' => ['longueur'=>['9,15,21']],
+        'type cheveux' => 'all',
+        'description' => 'Coloration de mèches',
+    ],
+    //BALAYAGE
+    'Balayage' => [
+        'prix' =>['longueur'=>['33,39,45']],
+        'type cheveux' => 'all',
+        'description' =>'Balayage Classique',
+    ],
+    //PERMANENTE
+    'Permanente' => [
+        'prix'=> 68,
+        'type cheveux' =>'all',
+        'description' =>'Frisage de cheveux',
+    ],
+    //Lissage
+    'Lissage Brésilien' => [
+        'prix' =>'longueur'=>['79,89,99'],
+        'type cheveux' => 'all',
+        'description' => 'soin des cheveux comblant le manque de kératine.',
+    ],
 ];
-
 $_type_cheveux = [
-    'longeur' => ['court', 'moyen', 'long'],
+    'longeur' => ['court', 'mi-long', 'long'],
     'qualité' => ['frisé', 'plat'],
-    'couleur' => ['roux', 'noir']
+    'couleur' => ['roux', 'noir', 'brun', 'blond']
 ];
