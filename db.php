0f6174e8576b1ae5cb29d6cc428e9b4d74b9f1bc
Zall9 <55153194+Zall9@users.noreply.github.com>
2020-12-02 10:51:06 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
-	$conn = mysqli_connect("localhost", "hairbook", "N9Ux46la", "hairbook");
+	$conn = mysqli_connect("127.0.0.1:3308", "hairbook", "N9Ux46la", "hairbook");
 	mysqli_set_charset($conn, "utf8");
 ?>
