da00ab267aa8abdf309e9e29472a5895ca646f1a
Zall9 <55153194+Zall9@users.noreply.github.com>
2020-12-02 13:00:09 +0100

    Update personnel.php

--- views/personnel.php
+++ views/personnel.php
@@ -1,30 +1,30 @@
 <section>
-	<h1>Présentation du personnel :</h1>
-	<div>
-		<div>
-			<h2>Directeur</h2>
-			<p>nomDirecteur</p>
-			<img src="<?= $_STATIC ?>/img/directeur.jpg"></img>
-		</div>
-		<div>
-			<h2>Employés :</h2>
-			<ul>
-				<li>
-					employé1
-				</li>
-				<li>
-					employé2
-				</li>
-				<li>
-					employé3
-				</li>
-				<li>
-					employé4
-				</li>
-				<li>
-					employé5
-				</li>
-			</ul>
-		</div>
-	</div>
+    <h1>Présentation du personnel :</h1>
+    <div>
+        <div>
+            <h2>Directeur</h2>
+            <p>nomDirecteur</p>
+            <img src="<?= $_STATIC ?>/img/directeur.jpg"></img>
+        </div>
+        <div>
+            <?php 
+                foreach ($_liste_coiffeur as $nom) {
+                    echo'<table class="employes">';
+                    echo '<tr>';
+                    echo '<th class="titre">'.array_keys($_liste_coiffeur, $key, TRUE)[0].'</th>';
+                    echo '</tr>';
+                    echo '<tr>';
+                    echo '<td class="nom">'.$key['nom'].'</td>';
+                    echo '<td class="prenom">'.$key['prenom'].'</td>';
+                    echo '</tr>';
+                    echo '<tr>';
+                    echo '<td'>.$key['specialite'].'</td>';
+                    echo '<td'>.$key['description'].'</td>';
+                    echo '</tr>';
+                    echo "</table>";
+                                                  }
+            ?>
+
+        </div>
+    </div>
 </section>
